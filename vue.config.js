f533bac66284dffa1e536752920a5f8b4f699aa8
1eonz <937896298@qq.com>
2026-09-20 16:24:45 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,135 @@
+'use strict'
+const path = require('path')
+const name = '警务协同后台管理系统' // page title
+const port = 30844 // dev port
+const isProd = process.env.NODE_ENV === 'production'
+
+const resolve = function(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  publicPath: isProd ? '/linkx/admin/' : '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: false,
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    // 不用每次启动都打开新页面
+    open: false,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    https: true,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        // target: `https://222.209.208.218:8016`,
+        target: `https://172.16.23.8:30844`,
+        // target: `https://222.209.208.218:8012`,
+        // target: `https://10.28.15.110:30014`,
+        // target: `https://10.28.54.176:30014`,
+        // target: `https://10.28.15.54:30014`,
+        // target: `https://10.28.54.81:30014`,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: '/linkx/admin'
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        // This line must in sass option
+        implementation: require('sass')
+      }
+    }
+  },
+  chainWebpack(config) {
+    config.plugins.delete('preload') // TODO: need test
+    config.plugins.delete('prefetch') // TODO: need test
+
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+
+    // set preserveWhitespace
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.compilerOptions.preserveWhitespace = true
+        return options
+      })
+      .end()
+
+    config
+      // https://webpack.js.org/configuration/devtool/#development
+      .when(!isProd, config => config.devtool('source-map'))
+
+    config.when(isProd, config => {
+      config
+        .plugin('ScriptExtHtmlWebpackPlugin')
+        .after('html')
+        .use('script-ext-html-webpack-plugin', [
+          {
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+            inline: /runtime\..*\.js$/
+          }
+        ])
+        .end()
+      config.optimization.splitChunks({
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial' // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: 'chunk-elementUI', // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+          },
+          commons: {
+            name: 'chunk-commons',
+            test: resolve('src/components'), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true
+          }
+        }
+      })
+      config.optimization.runtimeChunk('single')
+    })
+    // 缓存生成的 webpack 模块和 chunk，来改善构建速度
+    config.cache(true)
+  }
+}
