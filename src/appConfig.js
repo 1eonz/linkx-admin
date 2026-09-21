fc615a89fb4039d006d62df5fcde4a0429b05f82
1eonz <937896298@qq.com>
2026-09-21 10:42:28 +0800

    Create appConfig.js

---
+++ src/appConfig.js
@@ -0,0 +1,18 @@
+/**
+ * 应用全局配置参数
+ * 统一管理项目中的常量配置，方便后续修改
+ */
+
+/**
+ * 应用ID配置
+ * 1: 用户面应用 (H5)
+ * 2: 管理面应用 (Admin)
+ * 3: OpenSystem
+ * 4: NodeSystem
+ */
+export const APPLICATION_ID = {
+  H5: '1',
+  ADMIN: '2',
+  OPEN_SYSTEM: '3',
+  NODE_SYSTEM: '4'
+}
