20f9ffce048ba4da2ce2adec63f9d7c81524e65f
1eonz <937896298@qq.com>
2026-09-21 10:30:43 +0800

    Create vehicle.js

---
+++ src/locales/lang/en/vehicle.js
@@ -0,0 +1,10 @@
+export default {
+  name: 'The name of the vehicle',
+  number: 'License plate number',
+  code: 'The vehicle number',
+  type: 'Vehicle type',
+  nameNotEmpty: 'The vehicle name cannot be empty',
+  associatedEdgeGateway: 'Associated edge Gateway',
+  selectEdgeGateway: 'Select edge Gateway',
+  receiveVehicle: 'Recipients of vehicle'
+}
