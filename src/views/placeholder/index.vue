4ccf79b7b90ee8c9a5a0e8fa7b7ea98befa5a685
1eonz <937896298@qq.com>
2026-09-21 14:49:12 +0800

    Create index.vue

---
+++ src/views/placeholder/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="placeholder-container">
+    <div class="placeholder-content">
+      <i class="el-icon-warning-outline placeholder-icon"></i>
+      <h2>正在开发中</h2>
+      <p>该功能模块暂未上线，敬请期待</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Placeholder'
+}
+</script>
+
+<style lang="scss" scoped>
+.placeholder-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  min-height: 400px;
+}
+.placeholder-content {
+  text-align: center;
+  color: #909399;
+}
+.placeholder-icon {
+  font-size: 64px;
+  margin-bottom: 16px;
+}
+h2 {
+  font-size: 20px;
+  margin: 0 0 8px 0;
+  color: #606266;
+}
+p {
+  font-size: 14px;
+  margin: 0;
+}
+</style>
