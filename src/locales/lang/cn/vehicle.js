76197faaf165f6fd8d928d30343706f34a1042af
1eonz <937896298@qq.com>
2026-09-21 10:30:00 +0800

    Create vehicle.js

---
+++ src/locales/lang/cn/vehicle.js
@@ -0,0 +1,10 @@
+export default {
+  name: '车辆名称',
+  number: '车牌号码',
+  code: '车辆编号',
+  type: '车辆类型',
+  nameNotEmpty: '车辆名称不能为空',
+  associatedEdgeGateway: '关联边缘网关',
+  selectEdgeGateway: '选择边缘网关',
+  receiveVehicle: '领用车辆'
+}
