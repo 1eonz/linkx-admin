dd2161cdf009ffe20e4382a0356065f73b5293df
1eonz <937896298@qq.com>
2026-09-21 10:30:42 +0800

    Create mission.js

---
+++ src/locales/lang/en/mission.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'Name',
+  type: 'Type',
+  expression: 'Flow machine expression',
+  columns: 'Handle field dictionary',
+  view: 'Details',
+  statusMapping: 'State Machine',
+  i18nConf: 'I18n',
+  cannotEmpty: 'Required fields cannot be empty',
+  add: 'New configuration',
+  modify: 'Modify configuration',
+  version: 'Version',
+}
