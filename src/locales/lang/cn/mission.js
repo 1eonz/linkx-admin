f46d6be62d5e77d6a59ba0aaf8e0e33a953629b8
1eonz <937896298@qq.com>
2026-09-21 10:29:57 +0800

    Create mission.js

---
+++ src/locales/lang/cn/mission.js
@@ -0,0 +1,13 @@
+export default {
+  name: '名称',
+  type: '类型',
+  expression: '流程机表达式',
+  columns: '处置字段字典',
+  view: '详情',
+  statusMapping: '状态机',
+  i18nConf: '国际化',
+  cannotEmpty: '必填字段不能为空',
+  add: '新增配置',
+  modify: '修改配置',
+  version: '版本',
+}
